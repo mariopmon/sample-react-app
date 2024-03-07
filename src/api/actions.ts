9b6c50b6d8ccf8e95397bf7d7ff60fc614d636ac
mariopmon <161736021+mariopmon@users.noreply.github.com>
2024-03-07 15:27:38 +0000

    committed

--- src/api/actions.ts
+++ src/api/actions.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 
-const API_URL = "https://studious-train-7pq794x5gr72xrrv-3000.app.github.dev/api";
+const API_URL = "https://literate-couscous-4jqgg64j9ppwfj4jp-3000.app.github.dev/api";
 
 export const getWeatherData = async (city: string): Promise<WeatherData> => {
   return new Promise<WeatherData>((resolve, reject) => {
